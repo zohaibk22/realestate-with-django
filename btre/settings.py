f8323e863cfd0d2758c95aa64e4c1e757bebaad8
Zohaib <zohaibk1111@gmail.com>
2021-04-04 21:25:52 -0400

    Test:

--- btre/settings.py
+++ btre/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
