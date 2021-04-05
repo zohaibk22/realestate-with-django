c35ff22dc101f661230a65f1200d18ebc53442c2
Zohaib <zohaibk1111@gmail.com>
2021-04-05 00:48:57 -0400

    Listings page is set up

--- btre/settings.py
+++ btre/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
